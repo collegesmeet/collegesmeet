cad00eed73fbd63e72025f7377841d34805cf233
collegesmeet <94734371+collegesmeet@users.noreply.github.com>
2021-11-20 22:05:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ app = Flask(__name__)
 # run_with_ngrok(app) 
 
 @app.route("/", methods=['GET'])
-def log_in():
+def sign_in():
   f=open("sign_in/sign_in.html","r")
   text=f.read()
   f.close()
   return text
 
 @app.route("/sign_up", methods=['GET'])
-def log_in():
+def sign_up():
   f=open("signup/sign_up.html","r")
   text=f.read()
   f.close()
