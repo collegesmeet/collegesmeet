85189f704994fb88c107b5f58312032cd772546e
collegesmeet <94734371+collegesmeet@users.noreply.github.com>
2021-11-20 23:21:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def sign_up():
   f.close()
   return text
 
-@app.route("/sign_up_submit", methods=['POST'])
+@app.route("/sign_up_submit", methods=['GET'])
 def profile():
   f=open("profile/Profile.html","r")
   text=f.read()
