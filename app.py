d71c9144d23317e04df71c7016188d343e9f751c
collegesmeet <94734371+collegesmeet@users.noreply.github.com>
2021-11-20 21:55:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,14 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def log_in():
-  log_in=open("index.html","r")
-  text=log_in.read()
-  log_in.close()
+  f=open("sign_in/sign_in.html","r")
+  text=f.read()
+  f.close()
+  return text
+
+@app.route("/sign_up", methods=['GET'])
+def log_in():
+  f=open("signup/sign_up.html","r")
+  text=f.read()
+  f.close()
   return text
