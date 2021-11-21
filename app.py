836a03a1e6d2f53841b7b7a0a1d70cbd8aadc37b
collegesmeet <94734371+collegesmeet@users.noreply.github.com>
2021-11-22 01:31:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,14 +48,14 @@ def PROFILE_SUBMIT():
   except: email="can't_read"
   try: phone_number = request.form['phone_number']
   except: phone_number="can't_read"
-#   try: University = request.form['University']
-#   except: University="can't_read"
-#   try: Branch = request.form['Branch']
-#   except: Branch="can't_read"
-#   try: College = request.form['College']
-#   except: College="can't_read"
+  try: University = request.form['University']
+  except: University="can't_read"
+  try: Branch = request.form['Branch']
+  except: Branch="can't_read"
+  try: College = request.form['College']
+  except: College="can't_read"
   
-  text=f"first_name = {first_name} \n"+f"last_name = {last_name} \n"+f"password = {password} \n"+f"confirm_password = {confirm_password} \n"+f"University_Roll = {University_Roll} \n"+f"gender = {gender} \n"+f"email = {email} \n"+f"phone_number = {phone_number} \n"#+f"University = {University} \n"+f"Branch = {Branch} \n"++f"College = {College} \n"
+  text=f"first_name = {first_name} \n"+f"last_name = {last_name} \n"+f"password = {password} \n"+f"confirm_password = {confirm_password} \n"+f"University_Roll = {University_Roll} \n"+f"gender = {gender} \n"+f"email = {email} \n"+f"phone_number = {phone_number} \n"+f"University = {University} \n"+f"Branch = {Branch} \n"++f"College = {College} \n"
   
   return text
 
